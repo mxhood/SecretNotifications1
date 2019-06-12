5f835cfd7c8b1dd6fbe53c083d9a54e5439d8c0b
Maxine Hood <mhood@wellesley.edu>
2019-06-12 18:25:25 +0200

    all files

--- app/src/main/java/com/example/secretnotifications/MainActivity.java
+++ app/src/main/java/com/example/secretnotifications/MainActivity.java
@@ -4,37 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    static final String TAG = "myApp";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        setUpLaunch();
     }
 
-    private void setUpLaunch() {
-        Button btnStart = (Button) findViewById(R.id.btnStart);
-
-        btnStart.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Log.i(TAG, "this is a button log message");
-                Toast.makeText(getApplicationContext(), "Button clicked", Toast.LENGTH_SHORT)
-                        .show();
-
-                // Launch second activity
-                //Intent intent = new Intent(MainActivity.this, Running.class);
-                Intent intent = Running.makeIntent(MainActivity.this);
-                startActivity(intent);
-            }
-        });
+    public void setUpLaunch(View view) {
+        Intent intent = new Intent(this, Running.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/secretnotifications/Running.java
+++ app/src/main/java/com/example/secretnotifications/Running.java
@@ -2,11 +2,13 @@ package com.example.secretnotifications;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.NotificationManager;
 import android.content.Context;
 import android.content.Intent;
+import android.media.AudioManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 
 public class Running extends AppCompatActivity {
 
@@ -15,21 +17,70 @@ public class Running extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_running);
 
-        setupEndRunningMethod();
+        turnOnSilentMode(this);
+        turnOffScreen();
+        collectNotifications();
+        monitorTouch();
     }
 
-    private void setupEndRunningMethod() {
-        Button btnStart = (Button) findViewById(R.id.end_running);
-        btnStart.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                finish(); // terminates and return back to previous activity
-            }
-        });
+    protected void turnOnSilentMode(Context context) {
+        // Will prompt user to give permission to turn on Do Not Disturb
+        NotificationManager notificationManager =
+                (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M
+                && !notificationManager.isNotificationPolicyAccessGranted()) {
+
+            Intent intent = new Intent(
+                    android.provider.Settings
+                            .ACTION_NOTIFICATION_POLICY_ACCESS_SETTINGS);
+
+            startActivity(intent);
+        }
+
+        // Turn on Do not Disturb
+        AudioManager audioManager = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
+        try {
+            audioManager.setRingerMode(AudioManager.RINGER_MODE_SILENT);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            notificationManager.setInterruptionFilter(NotificationManager.INTERRUPTION_FILTER_NONE);
+        }
     }
 
+    private void turnOffScreen() {
 
-    public static Intent makeIntent(Context context) {
-        return new Intent(context, Running.class);
     }
+
+    private void collectNotifications() {
+
+    }
+
+    private void monitorTouch() {
+        boolean sensorTriggered = false;
+
+        if (sensorTriggered) {
+            sendResponse();
+        }
+    }
+
+    private void sendResponse() {
+        // analyze notifications
+        // buzz phone
+    }
+
+    public void endRunning(View view) {
+        // Turn off Do Not Disturb
+        AudioManager audioManager = (AudioManager) this.getSystemService(Context.AUDIO_SERVICE);
+        try {
+            audioManager.setRingerMode(AudioManager.RINGER_MODE_NORMAL);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        finish(); // terminates and return back to previous activity
+    }
+
 }
